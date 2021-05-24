8fcf32897d22d26191e3554b2c3cfc4cf1f056c4
TH0RlN <rarias-p@student.42madrid.com>
2021-05-24 19:39:57 +0200

    Primeiros pasos no minishell

---
+++ minishell.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rarias-p <rarias-p@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/24 18:04:42 by rarias-p          #+#    #+#             */
+/*   Updated: 2021/05/24 18:13:27 by rarias-p         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+
+int		get_next_line(int fd, char **line);
+char	*ft_strjoin(char const *s1, char const *s2);
+size_t	ft_strlen(const char *str);
+char	*ft_strdup(const char *s);
+char	*ft_strchr(const char *s, int c);
+char	*ft_substr(char const *s, unsigned int start, int len);
+int		ft_aux(char **line);
+int		ft_checker(int fd, char **line, char **file, int ret);
+
+#endif
